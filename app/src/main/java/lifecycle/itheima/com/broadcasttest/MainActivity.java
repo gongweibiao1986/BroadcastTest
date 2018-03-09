32074957ad2c900f8102146941c150789a7e535b
Yu haiyun <yuhy@valuefair.cn>
2018-03-09 10:43:14 +0800

    Initial commit

---
+++ app/src/main/java/lifecycle/itheima/com/broadcasttest/MainActivity.java
@@ -0,0 +1,12 @@
+package lifecycle.itheima.com.broadcasttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
